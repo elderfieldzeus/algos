a1e5f4e6fe0168c62d349f191626de14406f2132
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-02-25 14:16:51 +0800

    refactor: split tournament-sort into functions

--- 1-Sorting/12-Tournament/offline.c
+++ 1-Sorting/12-Tournament/offline.c
@@ -38,27 +38,8 @@ void tournamentSort(int arr[], int size) {
     }
 
     // Initialize tree, start from lowest-level parent and work upwards
-    for (int i = (tree_size - 2) / 2; i >= 0; i--) {
-
-        // Initiate left_trav, this will store the index of the current min of that subtree
-        int left_trav = i * 2 + 1;
-
-        // Initiate right_trav, this will store the index of the current min of that subtree
-        int right_trav = i * 2 + 2;
-
-        // If left_trav is currently not storing an index of an element, this means its child is not an element. So we STEAL the index from its child.
-        if (left_trav < elem_start) {
-            left_trav = tree[left_trav];
-        }
-
-        // If right_trav is currently not storing an index of an element, this means its child is not an element. So we STEAL the index from its child.
-        if (right_trav < elem_start) {
-            right_trav = tree[right_trav];
-        }
-
-        // Compare elements and store the index of the minimum element
-        tree[i] = (tree[right_trav] < tree[left_trav]) ? right_trav : left_trav;
-    }
+    // View '/12-Tournament/headers/tournament.h'
+    initTree(tree, tree_size, elem_start);
 
     // This is the start of tournament sort
     for (int i = 0; i < size; i++) {
@@ -73,25 +54,7 @@ void tournamentSort(int arr[], int size) {
         int parent = tree[0];
         
         // This is like heapify, but iterative
-        while (parent > 0) {
-            
-            // Traverse first to get the element's current parent
-            parent = (parent - 1) / 2;
-            
-            // Same as earlier, let the trav store the min element index of that subtree
-            int left_trav = parent * 2 + 1;
-            int right_trav = left_trav + 1;
-
-            if (left_trav < elem_start) {
-                left_trav = tree[left_trav];
-            }
-    
-            if (right_trav < elem_start) {
-                right_trav = tree[right_trav];
-            }
-
-            // Also same as earlier, let the parent now point to the index of the lesser element.
-            tree[parent] = (tree[right_trav] < tree[left_trav]) ? right_trav : left_trav;
-        }
+        // View '/12-Tournament/headers/tournament.h'
+        resetTree(tree, elem_start, parent);
     }
 }
