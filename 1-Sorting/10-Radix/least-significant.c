c85d13db53562e63f9646d705f9d7d3f21229697
elderfieldzeus <elderfieldzeus24@gmail.com>
2025-02-26 22:36:24 +0800

    chore: added comments to lsd

--- 1-Sorting/10-Radix/least-significant.c
+++ 1-Sorting/10-Radix/least-significant.c
@@ -20,6 +20,7 @@ void radixSort(int arr[], int size) {
     // This is like Cursor Based
     StackList storage = NULL;
 
+    // Initialize buckets
     for (int i = 0; i < BASE; i++) {
         buckets[i] = NULL;
     }
@@ -30,32 +31,45 @@ void radixSort(int arr[], int size) {
         push(&storage, -1);
     }
 
+    // Find max number
     int max_num = arr[0];
 
     for (int i = 1; i < size; i++) {
         max_num = max(max_num, arr[i]);
     }
 
+    // Find number of digits for max number
     int max_digits = log10(max_num) + 1;
 
+    // Radix sort
     for (int i = 0; i < max_digits; i++) {
+        // Find current place (1, 10, 100,...)
         int place = pow(BASE, i);
 
         for (int j = 0; j < size; j++) {
+            // Find index of bucket (This is the formula for getting the digit)
             int key = arr[j] / place % BASE;
 
+            // Get node from storage
             StackList temp = storage;
             storage = storage->next;
 
+            // Insert Sorted
             insertSortedAscNoMalloc(buckets + key, arr[j], temp);
         }
 
         int arr_index = 0;
         for (int i = 0; i < BASE; i++) {
             StackList *trav = buckets + i;
+
             while ((*trav) != NULL) {
+                // Store the head of the bucket
                 StackList temp = *trav;
+                
+                // Remove node from bucket
                 *trav = temp->next;
+
+                // Return data from bucket to array
                 arr[arr_index++] = temp->data;
 
                 // OPTIONAL: Return nodes to storage LL
