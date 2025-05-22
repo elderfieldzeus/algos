63eeead44e6774dfef216dd1f5c2693e2688fe2c
elderfieldzeus <elderfieldzeus24@gmail.com>
2025-05-22 15:09:37 +0800

    feat: added rbt delete MAY BE WRONG IDK

--- 2-Trees/03-Red-Black/rbt.c
+++ 2-Trees/03-Red-Black/rbt.c
@@ -16,8 +16,10 @@ void rotateLeft(RBT x);
 void rotateRight(RBT x);
 
 void insert(RBT *root, int data);
+
+void transplant(RBT *root, RBT toBeDeleted, RBT replacement);
 void delete(RBT *root, int data);
-void deleteFixup(RBT *root, RBT *node);
+void deleteFixup(RBT *root, RBT node);
 
 void preorder(RBT root);
 
@@ -31,11 +33,29 @@ int main() {
     insert(&root, 25);preorder(root);printf("\n");
     insert(&root, 21);preorder(root);printf("\n");
 
-    // delete(&root, 20);
+    delete(&root, 20);
+    printf("\nPreorder Traversal after deletion: ");
+    preorder(root);
 
-    // printf("Preorder Traversal after deletion: ");
-    // preorder(root);
-    // printf("\n");
+    delete(&root, 30);
+    printf("\nPreorder Traversal after deletion: ");
+    preorder(root);
+
+    delete(&root, 15);
+    printf("\nPreorder Traversal after deletion: ");
+    preorder(root);
+
+    delete(&root, 25);
+    printf("\nPreorder Traversal after deletion: ");
+    preorder(root);
+
+    delete(&root, 10);
+    printf("\nPreorder Traversal after deletion: ");
+    preorder(root);
+
+    delete(&root, 21);
+    printf("\nPreorder Traversal after deletion: ");
+    preorder(root);
 
     return 0;
 }
@@ -169,6 +189,148 @@ void insert(RBT *root, int data) {
     }
 
     (*root)->color = BLACK; // Case 1: Root is always black
+}
+
+void transplant(RBT *root, RBT toBeDeleted, RBT replacement) {
+    if (toBeDeleted == *root) {
+        *root = replacement;
+    }
+    else if (toBeDeleted->parent->left == toBeDeleted) {
+        toBeDeleted->parent->left = replacement;
+    } else {
+        toBeDeleted->parent->right = replacement;
+    }
+}
+
+void delete(RBT *root, int data) {
+    RBT *trav = root, nodeToBeDeleted = NULL, replacement = NULL;
+
+    // BST Traversal to find the node to be deleted
+    while (*trav != NULL && (*trav)->data != data) {
+        if (data < (*trav)->data) {
+            trav = &(*trav)->left;
+        } else {
+            trav = &(*trav)->right;
+        }
+    }
+
+    if (*trav == NULL) return;
+
+    nodeToBeDeleted = *trav;
+    int originalColor = nodeToBeDeleted->color;
+
+    // Case 1: Left Child is NULL
+    if (nodeToBeDeleted->left == NULL) {
+        replacement = nodeToBeDeleted->right;
+        transplant(root, nodeToBeDeleted, replacement);
+    }
+
+    // Case 2: Right Child is NULL
+    else if (nodeToBeDeleted->right == NULL) {
+        replacement = nodeToBeDeleted->left;
+        transplant(root, nodeToBeDeleted, replacement);
+    }
+
+    // Case 3: Both Children are not NULL
+    else {
+        replacement = nodeToBeDeleted->right;
+
+        while (replacement->left != NULL) {
+            replacement = replacement->left;
+        }
+
+        originalColor = replacement->color;
+
+        transplant(root, replacement, replacement->right);
+        
+        replacement->right = nodeToBeDeleted->right;
+        replacement->right->parent = replacement;
+
+        transplant(root, nodeToBeDeleted, replacement);
+        replacement->left = nodeToBeDeleted->left;
+        replacement->left->parent = replacement;
+        replacement->color = nodeToBeDeleted->color;
+    }
+
+
+    if (originalColor == BLACK && replacement != NULL) {
+        deleteFixup(root, replacement);
+    }
+    else if (originalColor == BLACK) {
+        // nodeToBeDeleted is used as a temporary node to signify DOUBLE_BLACK
+        deleteFixup(root, nodeToBeDeleted);
+    }
+
+    free(nodeToBeDeleted);
+}
+
+void deleteFixup(RBT *root, RBT node) {
+    if (*root == NULL || node == NULL) return;
+
+    while (node != *root && node->color == BLACK) {
+        bool nodeIsLeft = (node == node->parent->left || (node->parent->right != node && node->parent->left == NULL)) ? true : false;
+        RBT sibling = (nodeIsLeft) ? node->parent->right : node->parent->left;
+
+        if (sibling == NULL) {
+            node = node->parent;
+            continue;
+        }
+
+        // Case 1: Sibling is Red
+        if (sibling->color == RED) {
+            sibling->color = BLACK;
+            node->parent->color = RED;
+
+            if (nodeIsLeft) {
+                rotateLeft(node->parent);
+                sibling = node->parent->right;
+            } else {
+                rotateRight(node->parent);
+                sibling = node->parent->left;
+            }
+        }
+
+        // Case 2: Sibling is Black with two black children
+        if ((sibling->left == NULL || sibling->left->color == BLACK) && (sibling->right == NULL || sibling->right->color == BLACK)) {
+            sibling->color = RED;
+            node = node->parent;
+        }
+        else {
+            // Case 3.A: Node is left, Sibling is Black, and sibling's right child is black
+            if ((sibling->right == NULL || sibling->right->color == BLACK) && nodeIsLeft) {
+                sibling->color = RED;
+
+                sibling->left->color = BLACK;
+                rotateRight(sibling);
+                sibling = node->parent->right;
+
+            }
+            // Case 3.B: Node is right, Sibling is Black, and sibling's left child is black
+            else if ((sibling->left == NULL || sibling->left->color == BLACK) && !nodeIsLeft) {
+                sibling->color = RED;
+
+                sibling->right->color = BLACK;
+                rotateLeft(sibling);
+                sibling = node->parent->left;
+            }
+
+            // Case 4: Sibling is black and sibling's right child is Red
+            sibling->color = node->parent->color;
+            node->parent->color = BLACK;
+
+            if (nodeIsLeft) {
+                sibling->right->color = BLACK;
+                rotateLeft(node->parent);
+            } else {
+                sibling->left->color = BLACK;
+                rotateRight(node->parent);
+            }
+
+            node = *root;
+        }
+    }
+
+    node->color = BLACK;
 }
 
 void preorder(RBT root) {
